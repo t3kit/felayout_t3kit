e5d7eec9ac30ca9414fa2ca82aca0a3c6715023a
dmytro <dmytro@pixelant.se>
2016-01-19 12:40:03 +0200

    [FIX] small js fixes in header

--- dev/js/main/header/header.js
+++ dev/js/main/header/header.js
@@ -1,52 +1,15 @@
 // =================================
 // Global variables (jshint):
 
-    /*global touchSupport */
+    /*global*/
 // =================================
 
 jQuery(function($) {
     // Caching variables
     var $html = $('html');
-    var $body = $('body');
-    var $headerTop = $('.header-top');
     var $mainNavigation = $('.js__main-navigation');
     var $openSubMenuLink = $('.js__main-navigation__open-sub-menu-link');
-    var $navigationItemsWrp = $mainNavigation.find('.js__navigation__items-wrp');
     var $mainNavigationItemsList = $mainNavigation.find('.js__main-navigation__items-list').children('li');
-
-
-
-
-
-    // Bootstrap Hover Dropdown plugin. Execute  dropdown on no-touch screen > 992px.
-    // if (!touchSupport && $(window).width() > 992) {
-    //     $mainNavigation.find('.js__main-navigation__item-link').dropdownHover({ screenSize:992, delay:300 });
-    // }
-
-    // Freaze "body" when menu opens using "mobile-menu-collapsed". Aligning scroll bar on desktop.
-    // var mainNavSmallScreen = function mainNavSmallScreen() {
-    //     if ($(window).width() < 992) {
-    //         var bodyWidth = $body.width();
-    //         $navigationItemsWrp.on('show.bs.collapse', function() {
-    //             $html.addClass('mobile-menu-collapsed');
-    //             if (!touchSupport) {
-    //                 var menuWidth = $body.width();
-    //                 var bodyWidthFull = menuWidth - bodyWidth;
-    //                 $headerTop.css({
-    //                     'padding-right': bodyWidthFull
-    //                 });
-    //             }
-    //         });
-    //         $navigationItemsWrp.on('hide.bs.collapse', function() {
-    //             $html.removeClass('mobile-menu-collapsed');
-    //             if (!touchSupport) {
-    //                 $headerTop.css({
-    //                     'padding-right': 0
-    //                 });
-    //             }
-    //         });
-    //     }
-    // };
 
     // Cleanup function to clean unneeded classes
     var cleanup = function cleanup() {
@@ -67,29 +30,16 @@ jQuery(function($) {
         }
     });
 
-    // mainNavSmallScreen();
-
     $(window).on('orientationchange',function() {
-        // mainNavSmallScreen();
         cleanup();
         console.log('dfdsfs');
     });
-
-    // Navigation toggle button active class
-    // $('.js__pxa-nav-toggle-button').on('click', function(e) {
-    //     e.preventDefault();
-    //     $(this).toggleClass('active');
-    // });
-
-
-
 
     var mobileMenuAnimationComplete = true;
     $('.js__main-navigation__toggle-btn').on('click', function(e) {
         e.preventDefault();
         if (mobileMenuAnimationComplete) {
             mobileMenuAnimationComplete = false;
-            // $(this).toggleClass('_open');
             $html.toggleClass('mobile-menu-opened');
         }
         $('.js__navigation__items-wrp').not(':animated').slideToggle(300, function() {
@@ -97,15 +47,9 @@ jQuery(function($) {
         });
     });
 
-
-
-
-
-
 });
 
 // ====== class fo fixed main navigation bar   =======
-
 jQuery(function($) {
     var navbar = $('.js__main-navigation');
     var offsetTop = navbar.offset().top;
@@ -119,17 +63,7 @@ jQuery(function($) {
     });
 });
 
-
-
-
 jQuery(function($) {
-    // var $body = $('body');
-
-    // var $mainNavigation = $('.js__main-navigation');
-    // var $mainNavigationWrp = $mainNavigation.find('.js__main-navigation-min__items-wrp');
-    // var $mainNavigationItem = $mainNavigation.find('.js__main-navigation-min__item');
-    // var $mainNavigationMobileLink = $mainNavigation.find('.js__main-navigation-min__item-link-mobile');
-    // var mobileNavBackdrop = '.mobile-nav-backdrop';
     var $mainNavigationSearchBtn = $('.js__main-navigation__search-btn');
     var $mainNavigationSearchBox = $('.js__main-navigation__search-box');
     var $mainNavigationSearchBoxOverlay = $('.js__main-navigation__search-box-overlay');
@@ -138,9 +72,6 @@ jQuery(function($) {
     var $languageMenuBtn = $('.js__header-top__language-menu-btn');
     var $languageMenuBox = $('.js__header-top__language-menu-box');
     var $languageMenuBoxCloseBtn = $('.js__header-top__language-menu-box-close-btn');
-
-    // var $header = $('.js__header');
-    // var $headerInfo = $('.js__header-info').outerHeight();
 
     $mainNavigationSearchBtn.on('click', function(e) {
         e.preventDefault();
