b78f79c9244bf4442ebfb3476a0de02d9e43bff8
dmytro <dmytro@pixelant.se>
2016-02-03 19:00:49 +0200

    update

--- local.js
+++ local.js
@@ -1,5 +1,11 @@
-// typeahead.js
-//==========================================================
-// @import '../bower_components/typeahead.js/dist/typeahead.jquery.min.js';
-// @import 'local/typeahead.js';
-//==========================================================
+/*global mainSearchInputList:true*/
+
+// data for search suggest. For css styling
+$(document).ready(function() {
+    for (var key in mainSearchInputList) {
+        if (mainSearchInputList.hasOwnProperty(key)) {
+            mainSearchInputList[key]._list = ['map', 'home', 'content', 'Newsletter', 'subscription', 'tabs', 'accordion', 'carousel', 'icons', 'design', 'pxa', 'Pixelant', 'core', 't3layout', 'FElayout', 'bootstrap', 'pxa', 'search', 'contact', 'news', 'language', 'site', 'Pixalant', 'RTE', 'images', 'link', 'tablae', 'form', 'file', 'community', 'documentation', 'demo', 'customizer'];
+        }
+    }
+});
+
