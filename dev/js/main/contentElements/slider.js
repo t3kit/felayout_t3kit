a90200673bc54f550d10a196a151b18312d786b5
dmytro <dmytro@pixelant.se>
2016-02-23 18:24:21 +0200

    [FIX] fix image slider

--- dev/js/main/contentElements/slider.js
+++ dev/js/main/contentElements/slider.js
@@ -1,18 +1,24 @@
-jQuery(function($) {
-    $('.js__img-slider').swiper({
-        nextButton: '.js__img-slider__btn-next',
-        prevButton: '.js__img-slider__btn-prev',
-        pagination: '.js__img-slider__pagination',
-        paginationClickable: true,
-        preloadImages: false,
-        // loop: true,
-        // autoplay: 2500,
-        lazyLoading: true,
-        watchSlidesVisibility: true,
-        lazyLoadingInPrevNext: true,
-        speed: 800,
-        onLazyImageReady: function() {
-            $('.js__img-slider__caption-p').dotdotdot();
-        }
+(function($) {
+    'use strict';
+
+    // document load event
+    $(document).ready(function() {
+
+        // initialize swiper when document ready
+        $('.js__img-slider').swiper({
+            nextButton: '.js__img-slider__btn-next',
+            prevButton: '.js__img-slider__btn-prev',
+            pagination: '.js__img-slider__pagination',
+            paginationClickable: true,
+            preloadImages: false,
+            lazyLoading: true,
+            watchSlidesVisibility: true,
+            lazyLoadingInPrevNext: true,
+            speed: 800,
+            onLazyImageReady: function() {
+                $('.js__img-slider__caption-p').dotdotdot();
+            }
+        });
     });
-});
+
+})(jQuery);
