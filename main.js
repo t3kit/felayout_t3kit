341cb704360867f1c7401d7cf939349572edef7b
dmytro <dmytro@pixelant.se>
2016-05-07 14:27:27 +0300

    update

--- main.js
+++ main.js
@@ -187,6 +187,22 @@ jQuery(function($) {
 
 })(jQuery);
 
+(function($) {
+    'use strict';
+
+    // document load event
+    $(document).ready(function() {
+
+        // Parallax
+        // http://markdalgleish.com/projects/stellar.js
+        $.stellar({
+            horizontalScrolling: false,
+            verticalOffset: 40
+        });
+    });
+
+})(jQuery);
+
 
 /*global tx_solr_suggestUrl*/
 /*global Awesomplete*/
