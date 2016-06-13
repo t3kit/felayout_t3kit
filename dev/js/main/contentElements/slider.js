140ae6d3336906a2717bf25b69a5b68e071324ad
dmh <dmytro.hrynevych@gmail.com>
2016-06-13 16:48:55 +0300

    [FIX] Use tab to navigate slider

--- dev/js/main/contentElements/slider.js
+++ dev/js/main/contentElements/slider.js
@@ -1,3 +1,4 @@
+/* global Swiper*/
 (function($) {
     'use strict';
 
@@ -6,7 +7,7 @@
 
         // initialize swiper when document ready
         // http://idangero.us/swiper/api/
-        $('.js__img-slider').swiper({
+        var swiper = new Swiper('.js__img-slider', {
             nextButton: '.js__img-slider__btn-next',
             prevButton: '.js__img-slider__btn-prev',
             pagination: '.js__img-slider__pagination',
@@ -16,6 +17,15 @@
             watchSlidesVisibility: true,
             lazyLoadingInPrevNext: true,
             speed: 600
+        });
+        // Makes it possible to skip between slider images if they have links, using the tab button
+        swiper.container.on('focus', 'a', function(e) {
+            //Index of focused slide
+            var focusIndex = $(e.target).parents('.swiper-slide').index();
+            //Reset scrollLeft set by browser on focus
+            swiper.container.scrollLeft(0);
+            //Slide to focused slide
+            swiper.slideTo(focusIndex);
         });
     });
 
