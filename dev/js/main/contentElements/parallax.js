d18825e415252051ee977f63708517875757bccc
dmh <dmytro.hrynevych@gmail.com>
2016-06-13 12:37:54 +0300

    [FIX] Disabled parallax in IE since scrolling looks jerky

--- dev/js/main/contentElements/parallax.js
+++ dev/js/main/contentElements/parallax.js
@@ -5,18 +5,20 @@
     $(document).ready(function() {
         // Parallax
         // https://github.com/nk-o/jarallax
-        $('.parallax-img').jarallax({
-            type: 'scroll', //scroll, scale, opacity, scroll-opacity, scale-opacit
-            speed: 0.5,
-            noAndroid: false,
-            noIos: true
-        });
-        $('.parallax-video').jarallax({
-            type: 'scroll', //scroll, scale, opacity, scroll-opacity, scale-opacit
-            speed: 0.5,
-            noAndroid: true,
-            noIos: true
-        });
+        if (!$('html').hasClass('IE')) { // disabled in IE since scrolling looks jerky
+            $('.parallax-img').jarallax({
+                type: 'scroll', //scroll, scale, opacity, scroll-opacity, scale-opacit
+                speed: 0.5,
+                noAndroid: false,
+                noIos: true
+            });
+            $('.parallax-video').jarallax({
+                type: 'scroll', //scroll, scale, opacity, scroll-opacity, scale-opacit
+                speed: 0.5,
+                noAndroid: true,
+                noIos: true
+            });
+        }
     });
 
 })(jQuery);
