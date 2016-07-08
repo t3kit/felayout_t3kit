e46d2f66783d2df67fade41505ee24cc58e3bbe6
dmh <dmytro.hrynevych@gmail.com>
2016-07-08 11:50:48 +0300

    update

--- main.js
+++ main.js
@@ -281,6 +281,25 @@ jQuery(function($) {
 
 })(jQuery);
 
+// news Timeline
+$('.js__news-timeline__item').on('click', function(e) {
+     if ($(this).hasClass('collapsed')){
+         e.preventDefault();
+         $(this).removeClass('collapsed');
+         $(this).closest('.js__news-timeline__item-wrp').find('.js__news-timeline__date').addClass('open');
+     }
+ });
+
+// news Cards
+$('.js__news-cards__dotdotdot').dotdotdot({
+    watch: 'window'
+});
+
+// news Simple list
+$('.js__news-simple-list__dotdotdot').dotdotdot({
+    watch: 'window'
+});
+
 
 /*global tx_solr_suggestUrl*/
 /*global Awesomplete*/
