6444b791f72a27b8662fe973833b66852edf1a65
Viktoria Husaruk <vhusaruk92@gmail.com>
2016-11-30 14:46:22 +0200

    [FEATURE] Make the slider autoslide (Podio Feature_41) (#30)
    * [FEATURE] Make the slider autoslide (Podio Feature_41)
    
    * [FEATURE] Make the slider autoslide (Podio Feature_41)beta
    
    * [FEATURE] Make the slider autoslide (Podio Feature_41) with dmh's corrections
    
    * [FEATURE] Make the slider autoslide (Podio Feature_41) with dmh's corrections final
    
    * [STYLE] change slider.js according to .jscsrc

--- dev/js/main/contentElements/slider.js
+++ dev/js/main/contentElements/slider.js
@@ -7,32 +7,36 @@
 
         // initialize swiper when document ready
         // http://idangero.us/swiper/api/
-        var swiper = new Swiper('.js__img-slider', {
-            nextButton: '.js__img-slider__btn-next',
-            prevButton: '.js__img-slider__btn-prev',
-            pagination: '.js__img-slider__pagination',
-            paginationClickable: true,
-            preloadImages: false,
-            lazyLoading: true,
-            watchSlidesVisibility: true,
-            lazyLoadingInPrevNext: true,
-            speed: 600
-        });
-        // Makes it possible to skip between slider images if they have links, using the tab button
-        swiper.container.on('focus', 'a', function(e) {
-            //Index of focused slide
-            var focusIndex = $(e.target).parents('.swiper-slide').index();
-            //Reset scrollLeft set by browser on focus
-            swiper.container.scrollLeft(0);
+        var $elem = $('.js__img-slider');
+        $elem.each(function() {
+            var time = $(this).attr('data-autoplay');
+            var slider = new Swiper($(this), {
+                nextButton: '.js__img-slider__btn-next',
+                prevButton: '.js__img-slider__btn-prev',
+                pagination: '.js__img-slider__pagination',
+                paginationClickable: true,
+                preloadImages: false,
+                lazyLoading: true,
+                watchSlidesVisibility: true,
+                lazyLoadingInPrevNext: true,
+                speed: 600,
+                autoplay: time
+            });
+            // Makes it possible to skip between slider images if they have links, using the tab button
+            slider.container.on('focus', 'a', function(e) {
+                //Index of focused slide
+                var focusIndex = $(e.target).parents('.swiper-slide').index();
+                //Reset scrollLeft set by browser on focus
+                slider.container.scrollLeft(0);
 
-            // IE fix
-            setTimeout(function() {
-                swiper.container.scrollLeft(0);
-            }, 0);
+                // IE fix
+                setTimeout(function() {
+                    slider.container.scrollLeft(0);
+                }, 0);
 
-            //Slide to focused slide
-            swiper.slideTo(focusIndex);
+                //Slide to focused slide
+                slider.slideTo(focusIndex);
+            });
         });
     });
-
 })(jQuery);
